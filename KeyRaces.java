87f1b2ba9c2cabeb4a80570b068f02dcf0bf90a0
Shiven Khurana <93198336+ShivenKhurana@users.noreply.github.com>
2023-08-10 12:07:54 -0400

    Add files via upload

---
+++ KeyRaces.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class KeyRaces {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		//System.out.println("Enter an integer: ");
+		int words = scanner.nextInt();
+		//System.out.println("Enter an integer: ");
+		int p1time = scanner.nextInt();
+		//System.out.println("Enter an integer: ");
+		int p2time = scanner.nextInt();
+		//System.out.println("Enter an integer: ");
+		int p1ping = scanner.nextInt();
+		//System.out.println("Enter an integer: ");
+		int p2ping = scanner.nextInt();
+		int player1score = p1ping + words * p1time + p1ping;
+		int player2score = p2ping + words * p2time + p2ping;
+		if (player1score < player2score) {
+			System.out.println("First");
+		}
+		if (player1score > player2score) {
+			System.out.println("Second");
+		}
+		if (player1score == player2score) {
+			System.out.println("Friendship");	
+			}
+	}
+
+}
